592c6be16eea9230b7da4b8bf2bc34abb1ae5a09
Masood Sadat <masood.sdt@gmail.com>
2018-09-07 22:28:56 +0430

    add Q2g questions to subset

--- Other do-files/Fix MKP var names and subset.R
+++ Other do-files/Fix MKP var names and subset.R
@@ -39,7 +39,7 @@ mkp$simserial <- gsub("n/a", "", mkp$simserial)
 mkp$simserial <- gsub("[a-zA-Z]", "", mkp$simserial)
 
 # subset
-mkp <- select(mkp, hhid, sfdistrict, start, end, deviceid, simserial, contains("sfnumber"), contains("enum"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), Q4b1, Q4b2, Q5a, contains("Q2x"), Q5m2, Q5m3, Q5m4, Q5m5, Q5m6, Q5m7, Q5m8, Q5m9, Q5m10, Q5m1, Q10m6)
+mkp <- select(mkp, hhid, sfdistrict, start, end, deviceid, simserial, contains("sfnumber"), contains("enum"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), Q4b1, Q4b2, Q5a, contains("Q2x"), Q5m2, Q5m3, Q5m4, Q5m5, Q5m6, Q5m7, Q5m8, Q5m9, Q5m10, Q5m1, Q10m6)
 write_csv(mkp, paste0("~/Dropbox/SWEEP shared/Baseline QC Reports/Data/mkp_subset__", format(Sys.time(), "%d%b%Y"), ".csv"))
 
 
