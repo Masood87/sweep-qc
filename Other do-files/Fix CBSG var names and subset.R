dd793bd450b3227362142d3746af5407b7a78848
Masood Sadat <masood.sdt@gmail.com>
2018-09-13 11:18:18 +0430

    include all questions starting with Q10 to subset

--- Other do-files/Fix CBSG var names and subset.R
+++ Other do-files/Fix CBSG var names and subset.R
@@ -4,7 +4,7 @@ library(tidyverse)
 rm(list = ls())
 
 # import the data
-filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_CBSG_Final_2018_09_12_01_42_57_107118.csv"
+filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_CBSG_Final_2018_09_13_00_27_54_678788.csv"
 cbsg <- read_csv(filename)
 
 # parsing variable names and constract new variable names
@@ -48,7 +48,7 @@ cbsg$simserial <- gsub("n/a", "", cbsg$simserial)
 cbsg$simserial <- gsub("[a-zA-Z]", "", cbsg$simserial)
 
 # subset
-cbsg <- select(cbsg, hhid, sfdistrict, contains("enum"), Q1f1, contains("Q1l"), Q10cbsg89, Q1p, Q1r1, Q1r3, start, end, deviceid, simserial, contains("sfnumber"), Q2a, contains("Q2b"), contains("Q2l"), Q1n, contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), Q4b1, Q4b2, Q4c, Q5a, contains("Q6g1"), Q7g1, Q7f1, Q7f2, Q2_estimate, starts_with("Q2x"), starts_with("Q5m"), Q6a, Q6b, Q6c, Q6d, Q6e, Q6f, Q7k1, Q7n, Q10m6)
+cbsg <- select(cbsg, hhid, sfdistrict, contains("enum"), Q1f1, contains("Q1l"), Q1p, Q1r1, Q1r3, start, end, deviceid, simserial, contains("sfnumber"), Q2a, contains("Q2b"), contains("Q2l"), Q1n, contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), Q4b1, Q4b2, Q4c, Q5a, contains("Q6g1"), Q7g1, Q7f1, Q7f2, Q2_estimate, starts_with("Q2x"), starts_with("Q5m"), Q6a, Q6b, Q6c, Q6d, Q6e, Q6f, Q7k1, Q7n, starts_with("Q10"))
 write_csv(cbsg, paste0("~/Dropbox/SWEEP shared/Baseline QC Reports/Data/cbsg_subset__", format(Sys.time(), "%d%b%Y"), ".csv"))
 
 # export updated cbsg file
