45b363378192454694266dfd09d81bc094736375
Masood Sadat <masood.sdt@gmail.com>
2018-09-17 21:58:27 +0430

    update file names

--- Other do-files/Fix MKP var names and subset.R
+++ Other do-files/Fix MKP var names and subset.R
@@ -3,7 +3,7 @@ library(tidyverse)
 rm(list = ls())
 
 # import the data
-filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_MPK_Final_2018_09_15_03_05_21_004324.csv"
+filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_MPK_Final_2018_09_17_01_17_38_307811.csv"
 mkp <- read_csv(filename)
 
 # parsing variable names and constract new variable names
