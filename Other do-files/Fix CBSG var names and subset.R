43658041a3c4f78cd4dc697164d7726e62687fec
Masood Sadat <masood.sdt@gmail.com>
2018-09-28 15:06:18 +0430

    r codes are generated from do-file automatically

--- Other do-files/Fix CBSG var names and subset.R
+++ Other do-files/Fix CBSG var names and subset.R
@@ -1,56 +1,34 @@
-### fixing CBSG variable names
+library(tidyverse) 
+filename <- "SWEEP_CBSG_Final_2018_09_28_03_52_43_397332.csv" 
+cbsg <- read_csv(paste0("/Users/macbookair/Dropbox/SWEEP shared/Baseline QC Reports/Data/", filename)) 
 
-library(tidyverse)
-rm(list = ls())
+# parsing variable names and constract new variable names 
+fullnames <- colnames(cbsg) 
+last1 <- word(fullnames, -1, sep = "/") 
+last2 <- word(fullnames, -2, sep = "/") 
+vnames <- last1 
+numerics = !is.na(as.numeric(vnames)) 
+vnames[numerics] <- paste(last2[numerics], last1[numerics], sep = "_") 
+inside_pranth_1 <- gsub(".*?\\[(.*?)\\].*", "\\1", fullnames, perl=TRUE) %>% as.numeric() %>% as.character() 
+inside_pranth_2 <- gsub(".*\\[(.*)\\].*", "\\1", fullnames, perl=TRUE) %>% as.numeric() %>% as.character() 
+dup <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE) 
+vnames[dup] <- paste(vnames[dup], inside_pranth_1[dup], sep = "_") 
+dup2 <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE) 
+vnames[dup2] <- paste(vnames[dup2], inside_pranth_2[dup2], sep = "_") 
 
-# import the data
-filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_CBSG_Final_2018_09_23_02_17_52_565455.csv"
-cbsg <- read_csv(filename)
+# Assemble all the data in a dataframe 
+newvarnames <- data.frame(oldnames=fullnames, newnames=vnames, numerics=numerics, dup=dup, dup2=dup2, inside_pranth_1=inside_pranth_1, inside_pranth_2=inside_pranth_2) 
 
-# parsing variable names and constract new variable names
-fullnames <- colnames(cbsg)
-last1 <- word(fullnames, -1, sep = "/")
-last2 <- word(fullnames, -2, sep = "/")
-vnames <- last1
-numerics = !is.na(as.numeric(vnames))
-vnames[numerics] <- paste(last2[numerics], last1[numerics], sep = "_")
-inside_pranth_1 <- gsub(".*?\\[(.*?)\\].*", "\\1", fullnames, perl=TRUE) %>%
-  as.numeric() %>% as.character()
-inside_pranth_2 <- gsub(".*\\[(.*)\\].*", "\\1", fullnames, perl=TRUE) %>%
-  as.numeric() %>% as.character()
-dup <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE)
-vnames[dup] <- paste(vnames[dup], inside_pranth_1[dup], sep = "_")
-dup2 <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE)
-vnames[dup2] <- paste(vnames[dup2], inside_pranth_2[dup2], sep = "_")
+# rename cbsg file with new var names 
+colnames(cbsg) <- newvarnames$newnames 
 
-# Assemble all the data in a dataframe
-newvarnames <- data.frame(oldnames = fullnames, 
-           newnames = vnames, 
-           numerics = numerics,
-           dup = dup,
-           dup2 = dup2,
-           inside_pranth_1 = inside_pranth_1,
-           inside_pranth_2 = inside_pranth_2)
+# remove alpha characters from simserial variable 
+cbsg$simserial <- gsub("n/a", "", cbsg$simserial) 
+cbsg$simserial <- gsub("[a-zA-Z]", "", cbsg$simserial) 
 
-# export the old var names and new var names to a csv
-# newvarnames_subset <- select(newvarnames, oldnames, newnames)
-# newvarnames_subset$statanames <- gsub("[[:punct:]]", "", newvarnames_subset$oldnames)
-# mutate(newvarnames_subset, 
-#        `*rename` = paste("cap rename", statanames, newnames)) %>%
-#   select(`*rename`) %>%
-#   write_csv("~/Downloads/sweep/cleaning and labelling/variable names/cbsg_variable_names.do")
+# remove objects 
+# rm(list = c("fullnames", "last1", "last2", "vnames", "numerics", "inside_pranth_1", "inside_pranth_2", "dup", "dup2", "filename", "newvarnames")) 
 
-# rename cbsg file with new var names
-colnames(cbsg) <- newvarnames$newnames
-
-# remove alpha characters from simserial variable
-cbsg$simserial <- gsub("n/a", "", cbsg$simserial)
-cbsg$simserial <- gsub("[a-zA-Z]", "", cbsg$simserial)
-
-# remove objects
-rm(list = c("fullnames", "last1", "last2", "vnames", "numerics", "inside_pranth_1", "inside_pranth_2", "dup", "dup2", "filename", "newvarnames"))
-
-# subset
-cbsg <- select(cbsg, start:Q2b_1, starts_with("Q2b"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), starts_with("Q3a"), starts_with("Q3c"), starts_with("Q3f"), Q4b1, Q4b2, Q4c, Q5a, contains("Q6g1"), Q7g1, Q7f1, Q7f2, Q2_estimate, starts_with("Q2x"), starts_with("Q5m"), Q6a, Q6b, Q6c, Q6d, Q6e, Q6f, Q7k1, Q7n, starts_with("Q10"), Start_time, end_time) %>% rename(start_time = Start_time)
-write_csv(cbsg, paste0("~/Dropbox/SWEEP shared/Baseline QC Reports/Data/cbsg_subset__", format(Sys.time(), "%d%b%Y"), ".csv"), na = "")
-
+# subset 
+cbsg <- select(cbsg, start:Q2b_1, starts_with("Q2b"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), starts_with("Q3a"), starts_with("Q3c"), starts_with("Q3f"), Q4b1, Q4b2, Q4c, Q5a, contains("Q6g1"), Q7g1, Q7f1, Q7f2, Q2_estimate, starts_with("Q2x"), starts_with("Q5m"), Q6a, Q6b, Q6c, Q6d, Q6e, Q6f, Q7k1, Q7n, starts_with("Q10"), Start_time, end_time) %>% rename(start_time = Start_time) 
+write_csv(cbsg, paste0("/Users/macbookair/Dropbox/SWEEP shared/Baseline QC Reports/Data/cbsg_subset__", format(Sys.time(), "%d%b%Y"), ".csv"), na = "") 
--- Other do-files/Fix MKP var names and subset.R
+++ Other do-files/Fix MKP var names and subset.R
@@ -1,48 +1,34 @@
-### fixing MKP variable names + subsetting
-library(tidyverse)
-rm(list = ls())
+library(tidyverse) 
+filename <- "SWEEP_MPK_Final_2018_09_28_03_23_10_954956.csv" 
+mkp <- read_csv(paste0("/Users/macbookair/Dropbox/SWEEP shared/Baseline QC Reports/Data/", filename)) 
 
-# import the data
-filename <- "~/Dropbox/SWEEP shared/Baseline QC Reports/Data/SWEEP_MPK_Final_2018_09_23_02_12_26_078174.csv"
-mkp <- read_csv(filename)
+# parsing variable names and constract new variable names 
+fullnames <- colnames(mkp) 
+last1 <- word(fullnames, -1, sep = "/") 
+last2 <- word(fullnames, -2, sep = "/") 
+vnames <- last1 
+numerics = !is.na(as.numeric(vnames)) 
+vnames[numerics] <- paste(last2[numerics], last1[numerics], sep = "_") 
+inside_pranth_1 <- gsub(".*?\\[(.*?)\\].*", "\\1", fullnames, perl=TRUE) %>% as.numeric() %>% as.character() 
+inside_pranth_2 <- gsub(".*\\[(.*)\\].*", "\\1", fullnames, perl=TRUE) %>% as.numeric() %>% as.character() 
+dup <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE) 
+vnames[dup] <- paste(vnames[dup], inside_pranth_1[dup], sep = "_") 
+dup2 <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE) 
+vnames[dup2] <- paste(vnames[dup2], inside_pranth_2[dup2], sep = "_") 
 
-# parsing variable names and constract new variable names
-fullnames <- colnames(mkp)
-last1 <- word(fullnames, -1, sep = "/")
-last2 <- word(fullnames, -2, sep = "/")
-vnames <- last1
-numerics = !is.na(as.numeric(vnames))
-vnames[numerics] <- paste(last2[numerics], last1[numerics], sep = "_")
-inside_pranth_1 <- gsub(".*?\\[(.*?)\\].*", "\\1", fullnames, perl=TRUE) %>%
-  as.numeric() %>% as.character()  
-inside_pranth_2 <- gsub(".*\\[(.*)\\].*", "\\1", fullnames, perl=TRUE) %>%
-  as.numeric() %>% as.character()
-dup <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE)
-vnames[dup] <- paste(vnames[dup], inside_pranth_1[dup], sep = "_")
-dup2 <- duplicated(vnames) | duplicated(vnames, fromLast = TRUE)
-vnames[dup2] <- paste(vnames[dup2], inside_pranth_2[dup2], sep = "_")
+# Assemble all the data in a dataframe 
+newvarnames <- data.frame(oldnames=fullnames, newnames=vnames, numerics=numerics, dup=dup, dup2=dup2, inside_pranth_1=inside_pranth_1, inside_pranth_2=inside_pranth_2) 
 
-# Assemble all the data in a dataframe
-newvarnames <- data.frame(oldnames = fullnames, 
-                          newnames = vnames, 
-                          numerics = numerics,
-                          dup = dup,
-                          dup2 = dup2,
-                          inside_pranth_1 = inside_pranth_1,
-                          inside_pranth_2 = inside_pranth_2)
+# rename mkp file with new var names 
+colnames(mkp) <- newvarnames$newnames 
 
-# rename cbsg file with new var names
-colnames(mkp) <- newvarnames$newnames
+# remove alpha characters from simserial variable 
+mkp$simserial <- gsub("n/a", "", mkp$simserial) 
+mkp$simserial <- gsub("[a-zA-Z]", "", mkp$simserial) 
 
-# remove alpha characters from simserial variable
-mkp$simserial <- gsub("n/a", "", mkp$simserial)
-mkp$simserial <- gsub("[a-zA-Z]", "", mkp$simserial)
+# remove objects 
+# rm(list = c("fullnames", "last1", "last2", "vnames", "numerics", "inside_pranth_1", "inside_pranth_2", "dup", "dup2", "filename", "newvarnames")) 
 
-# remove objects
-rm(list = c("fullnames", "last1", "last2", "vnames", "numerics", "inside_pranth_1", "inside_pranth_2", "dup", "dup2", "filename", "newvarnames"))
-
-# subset
-mkp <- select(mkp, start:Q2b_1, starts_with("Q2b"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), Q2_estimate, contains("Q2x"), starts_with("Q3a"), starts_with("Q3c"), starts_with("Q3f"), Q4b1, Q4b2, Q5a, Q5m2, Q5m3, Q5m4, Q5m5, Q5m6, Q5m7, Q5m8, Q5m9, Q5m10, Q5m1, starts_with("Q10"), start_time, end_time)
-write_csv(mkp, paste0("~/Dropbox/SWEEP shared/Baseline QC Reports/Data/mkp_subset__", format(Sys.time(), "%d%b%Y"), ".csv"))
-
-
+# subset 
+mkp <- select(mkp, start:Q2b_1, starts_with("Q2b"), contains("Q2l"), contains("Q2n1"), contains("Q2n2"), starts_with("Q2g"), Q2_estimate, contains("Q2x"), starts_with("Q3a"), starts_with("Q3c"), starts_with("Q3f"), Q4b1, Q4b2, Q5a, Q5m2, Q5m3, Q5m4, Q5m5, Q5m6, Q5m7, Q5m8, Q5m9, Q5m10, Q5m1, starts_with("Q10"), start_time, end_time) 
+write_csv(mkp, paste0("~/Dropbox/SWEEP shared/Baseline QC Reports/Data/mkp_subset__", format(Sys.time(), "%d%b%Y"), ".csv"), na = "") 
